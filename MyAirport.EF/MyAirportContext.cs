b970d0219618422004d7c9eb2fd567bbef3045eb
Francoois <francoois.louis@hotmail.fr>
2020-02-24 17:41:39 +0100

    Add Logger to DBContext

--- MyAirport.EF/MyAirportContext.cs
+++ MyAirport.EF/MyAirportContext.cs
@@ -4,11 +4,20 @@ using System.Text;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace FLS.MyAirport.EF
 {
     public class MyAirportContext : DbContext
     {
+        //https://docs.microsoft.com/fr-fr/ef/core/miscellaneous/logging?tabs=v3
+        public static readonly ILoggerFactory MyAirportLoggerFactory 
+            = LoggerFactory.Create(builder => { builder.AddConsole(); }); // Simple factory , il faut créer le logger 
+
+        // https://docs.microsoft.com/fr-fr/ef/core/miscellaneous/logging?tabs=v3
+        //Créer mon propre logger dédié pour y balancer du log 
+
+
         public DbSet<Bagage> Bagages { get; set; }
         public DbSet<Vol> Vols { get; set; }
 
@@ -18,6 +27,10 @@ namespace FLS.MyAirport.EF
             //optionsBuilder.UseSqlServer(
             //    @"Server=(localdb)\mssqllocaldb;Database=Airport;Integrated Security=True");
             optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["MyAirportDatabase"].ConnectionString);
+
+            //Pas logique que lui aille chercher l'option dans le fichier de conf... étape d'après
+            // Pas logique que le connecteur sur la bdd ait la charge d'aller chercher la chaine de connection
+            optionsBuilder.UseLoggerFactory(MyAirportLoggerFactory); // Utiise la factory pour gérer tes logs. Utilise une instance de ILogger
         }
     }
 }
